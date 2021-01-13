fa7f5493bc32d9dec9542dcada076b94d46a91b0
Dario Martinez <gusdamtzr@gmail.com>
2021-01-12 18:35:59 -0600

    Set up react router dom for the navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import Navbar from './Navbar.js';
 import React from 'react';
 
@@ -6,11 +8,13 @@ class App extends React.Component {
   render(){
     return (
       <div>
+        <Router>
         <header>
           <Navbar />
         </header>
         <main>Content</main>
         <footer>Links and other stuff</footer>
+        </Router>
       </div>
     );
   }
