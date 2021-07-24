43122d59e6d7424e9821364bf00f18a75a2e9358
frmdz <33354499+frmdz@users.noreply.github.com>
2021-07-23 19:43:36 -0500

    update link to post_id

--- src/Company.js
+++ src/Company.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Company({ link, image, name }) {
+function Company({ post_id, image, name }) {
   return (
     <div className="col-6 col-md-4 col-lg-3 text-center">
-      <Link to={link}>
+      <Link to={`/post/${post_id}`}>
         <img src={image} alt={name} className="img-fluid btn-brand" />
       </Link>
     </div>
