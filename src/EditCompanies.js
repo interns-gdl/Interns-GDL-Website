9d2624c32038c26a9109fa101feabb1863d31687
frmdz <33354499+frmdz@users.noreply.github.com>
2021-07-25 19:46:47 -0500

    update collection name[F

--- src/EditCompanies.js
+++ src/EditCompanies.js
@@ -3,22 +3,24 @@ import CompaniesTable from './CompaniesTable.js'
 import CompaniesForm from './CompaniesForm.js'
 import {read, remove, update} from './modules/firestore.js'
 
+const COLLECTION = "test-companies"
+
 function EditCompanies() {
 
   const [companies, setCompanies] = useState([]);
 
   async function readCompanies() {
-    const companies = await read("companies");
+    const companies = await read(COLLECTION);
     setCompanies(companies);
   }
 
  async function updateCompanies(id, object) {
-    const companies = await update("companies", id, object);
+    const companies = await update(COLLECTION, id, object);
     setCompanies(companies);
   }
 
   async function removeCompanies(id) {
-    const companies = await remove("companies", id);
+    const companies = await remove(COLLECTION, id);
     setCompanies(companies);
   }
 
