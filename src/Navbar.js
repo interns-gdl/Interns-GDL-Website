780a38662c8a788220d04770ccbc00082998ce48
frmdz <33354499+frmdz@users.noreply.github.com>
2021-08-27 15:00:10 -0500

    turns Navbar into a functional component and gets the elements and links dynamically

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,46 +3,49 @@ import {Link} from 'react-router-dom';
 
 import Logo from './Logo.js';
 
-class Navbar extends React.Component{
-  render(){
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-indigo">
-        <div className="container-fluid ms-0 ps-0">
-          <Link to="/" className="text-decoration-none">
-            <span className="navbar-brand text-dark  text-brand rounded-pill bg-light px-3">
-              <Logo width="30" height="30" className="me-2 d-inline-block align-top"/>
-              Interns GDL
-            </span>
-          </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+function NavarElement({ text, link }) {
+  return (
+    <Link to={link} className="text-decoration-none">
+      <span className="nav-link">{text}</span>
+    </Link>
+  )
+}
+
+function Navbar() {
+
+  const elements = [
+    { text: "Get an Intership", link: "/apply" },
+    { text: "Workshops", link: "/workshops" },
+    { text: "Events", link: "/events" },
+    { text: "Resources", link: "/resources" },
+    { text: "Fun", link: "/fun" },
+    { text: "About Us", link: "/about" }
+  ];
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-indigo">
+      <div className="container-fluid ms-0 ps-0">
+
+        <Link to="/" className="text-decoration-none">
+          <span className="navbar-brand text-dark  text-brand rounded-pill bg-light px-3">
+            <Logo width="30" height="30" className="me-2 d-inline-block align-top" />
+            Interns GDL
+          </span>
+        </Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Link to="/apply" className="text-decoration-none">
-              <span className="nav-link">Get an Internship</span>
-            </Link>
-            <Link to="/workshops" className="text-decoration-none">
-              <span className="nav-link">Workshops</span>
-            </Link>
-            <Link to="/events" className="text-decoration-none">
-              <span className="nav-link">Events</span>
-            </Link>
-            <Link to="/resources" className="text-decoration-none">
-              <span className="nav-link">Resources</span>
-            </Link>
-            <Link to="/fun" className="text-decoration-none">
-              <span className="nav-link">Fun</span>
-            </Link>
-            <Link to="/about" className="text-decoration-none">
-              <span className="nav-link">About us</span>
-            </Link>
-          </div>
+            { elements.map((elem) => <NavarElement text={elem.text} link={elem.link} key={elem.text}/>) }
           </div>
         </div>
-      </nav>
-    )
-  }
+
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
